b6e438d465cd62d9d632a4f192c0936475459684
Douae <douae4344@gmail.com>
2026-08-29 18:33:31 +0100

    Verification email: ajoute VerificationCodeMail et son template (Etape C)

---
+++ app/Mail/VerificationCodeMail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationCodeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $code;
+
+    /**
+     * @param string $code Le code à 6 chiffres à afficher dans l'email.
+     */
+    public function __construct(string $code)
+    {
+        $this->code = $code;
+    }
+
+    public function build()
+    {
+        return $this->subject('Votre code de vérification CampusConnect')
+            ->view('emails.verification-code');
+    }
+}
