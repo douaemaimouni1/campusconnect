3d0e7e73313757dad92e6cb57198e9a72de3fbe7
Douae <douae4344@gmail.com>
2026-08-31 18:07:53 +0100

    fix: applique EnsureUserIsNotBanned au groupe web pour proteger les actions Livewire (wire:click et polling) d'un utilisateur banni avec un onglet deja ouvert

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'superadmin' => \App\Http\Middleware\EnsureUserIsSuperAdmin::class,
             'not.banned' => \App\Http\Middleware\EnsureUserIsNotBanned::class,
         ]);
+
+        $middleware->web(append: [
+            \App\Http\Middleware\EnsureUserIsNotBanned::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
